40d8f2a07315f58568cfbd15ac30b1f293d284bf
GJT-npu <gjt_npu@163.com>
2025-08-21 23:29:55 +0800

    三、纯虚函数和抽象类

--- test9/main.cpp
+++ test9/main.cpp
@@ -57,30 +57,63 @@ using namespace std;
 *	2、虚函数允许在派生类中重写基类的函数。
 *	3、虚函数指针的是通过基类指针或引用调用的函数。
 */
+//class Animal {
+//public:
+//	virtual void sound() {
+//		cout << "Animal makes a sound" << endl;
+//	}
+//	virtual void eat() {
+//		cout << "Animal eats" << endl;
+//	}
+//};
+//
+//class Cat :public Animal {
+//public:
+//	void sound() override {
+//		cout << "喵" << endl;
+//	}
+//};
+//
+//int main() {
+//
+//	Cat c;
+//	c.sound();
+//	c.Animal::sound();
+//
+//	cout << "Size of Animal:" << sizeof(Animal) << endl;
+//
+//	return 0;
+//}
+
+/*
+* 三、纯虚函数和抽象类
+*	1、纯虚函数是指在基类中声明但不实现的虚函数。
+*   2、纯虚函数的声明形式为：virtual void functionName() = 0;
+*   3、包含纯虚函数的类称为抽象类，不能实例化。
+*	4、派生类必须实现所有的纯虚函数才能实例化。否则也是抽象类 不能实例化
+*/
 class Animal {
 public:
-	virtual void sound() {
-		cout << "Animal makes a sound" << endl;
-	}
-	virtual void eat() {
-		cout << "Animal eats" << endl;
-	}
-};
+	virtual void sound() = 0; // 纯虚函数
 
+};
 class Cat :public Animal {
 public:
+
+	Cat() : m_age(0) { }
+
 	void sound() override {
 		cout << "喵" << endl;
 	}
+
+private:
+	int m_age;
 };
 
 int main() {
-
+	
 	Cat c;
 	c.sound();
-	c.Animal::sound();
-
-	cout << "Size of Animal:" << sizeof(Animal) << endl;
 
 	return 0;
 }
