439a2e83f3a695f8a8cdd930c1cd7f2062d61612
GJT-npu <gjt_npu@163.com>
2025-08-21 21:24:12 +0800

    test8
    继承

---
+++ test8/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+/*
+* 继承
+* 
+* class 子类 : 继承方式 父类 { }
+* 子类 ： 派生类
+* 父类 ： 基类
+* 继承方式 ： public / protected / private
+* 
+* 3*3=9
+*                 |                     父类访问权限                 |
+* 继				  |	    public     |	 protected    |   private   |
+* 承		public    |     public     |	 protected    |   无法访问   |
+* 方		protected |     protected  |	 protected    |   无法访问   |
+* 式		private   |     private    |	 private      |   无法访问   |
+* 
+*/
+
+int main() {
+
+	return 0;
+}
