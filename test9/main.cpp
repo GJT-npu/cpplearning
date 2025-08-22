9b198895ce3dc4db5abba6282504fa0b955e248c
GJT-npu <gjt_npu@163.com>
2025-08-22 12:35:07 +0800

    四、虚析构与纯虚析构

--- test9/main.cpp
+++ test9/main.cpp
@@ -92,28 +92,94 @@ using namespace std;
 *   3、包含纯虚函数的类称为抽象类，不能实例化。
 *	4、派生类必须实现所有的纯虚函数才能实例化。否则也是抽象类 不能实例化
 */
+//class Animal {
+//public:
+//	virtual void sound() = 0; // 纯虚函数
+//
+//};
+//class Cat :public Animal {
+//public:
+//
+//	Cat() : m_age(0) { }
+//
+//	void sound() override {
+//		cout << "喵" << endl;
+//	}
+//
+//private:
+//	int m_age;
+//};
+//
+//int main() {
+//	
+//	Cat c;
+//	c.sound();
+//
+//	return 0;
+//}
+
+/*
+* 四、虚析构与纯虚析构
+*	1、防止内存泄漏：当基类指针指向派生类对象时，如果基类的析构函数不是虚函数，可能导致派生类的析构函数无法被调用，从而造成内存泄漏。
+*	2、纯虚析构函数：在抽象类中，可以将析构函数声明为纯虚函数，以确保派生类必须实现析构函数。
+* 	3、虚析构函数：在基类中声明析构函数为虚函数，以确保在删除基类指针时，能够正确调用派生类的析构函数。
+*   4、当父类指针指向派生类对象时，它实际上指向的是该对象中的父类子对象部分
+*/
 class Animal {
 public:
+	Animal(): size(NULL) { 
+		size = new int(100); // 假设size是一个指向int的指针
+		cout << "Animal构造函数" << endl; 
+	} 
+
+	virtual ~Animal() { 
+		if (size) {
+			delete size;
+			size = NULL;
+		}
+		cout << "Animal虚析构函数" << endl; 
+	} 
+
 	virtual void sound() = 0; // 纯虚函数
 
+	int *size;
 };
 class Cat :public Animal {
 public:
 
-	Cat() : m_age(0) { }
+	Cat() : m_age(NULL) {
+		m_age = new int(10);
+		cout << "Cat构造函数" << endl;
+	}
+	~Cat() {
+		if (m_age) {
+			delete m_age;
+			m_age = NULL;
+		}
+		cout << "Cat虚析构函数" << endl;
+	}
 
-	void sound() override {
+	void sound() {
 		cout << "喵" << endl;
 	}
 
-private:
-	int m_age;
+	int *m_age;
 };
 
 int main() {
-	
-	Cat c;
-	c.sound();
+
+	//Cat c;
+	//cout << *c.m_age << endl;
+
+	// 指针a 访问不到子类的成员变量 
+	// 当父类指针指向派生类对象时，它实际上指向的是该对象中的父类子对象部分
+	Animal *a = new Cat();
+	cout << *a->size << endl;
+	delete a;
+
+	Cat *c = new Cat();
+	cout << *c->m_age << endl;
+	delete c;
 
 	return 0;
 }
