af064cf1d3ce810f1c272a30549844d25b476dbb
GJT-npu <gjt_npu@163.com>
2025-08-23 10:31:50 +0800

    * 四、类模板的继承 * 1、直接指定类型 * 2、类模板化

--- test10/main.cpp
+++ test10/main.cpp
@@ -35,9 +35,7 @@ using namespace std;
 
 /*
 * 二、类模板
-*/
-
-/*
+*
 * 1、实现动态数组类
 * 2、并改用类模板来实现
 * 3、成员函数类外实现
@@ -87,16 +85,63 @@ using namespace std;
 //	return 0;
 //}
 
-
-
 /*
 * 三、类模板对象的函数传参
 *	1、直接指定类型
 * 	2、参数模板化
 *   3、类模板化
 */
+//template<class NameType, class HpType>
+//class Hero {
+//public:
+//	Hero(NameType name , HpType hp) : m_name(name) , m_hp(hp) {
+//
+//	}
+//
+//	void ShowHero() {
+//		cout << "Name: " << m_name << " , Hp: " << m_hp << endl;
+//	}
+//
+//private:
+//	NameType m_name;
+//	HpType m_hp;
+//};
+//
+////1、直接指定类型
+//void test1(Hero<string , double> &h) {
+//	h.ShowHero();
+//}
+//
+////2、参数模板化
+//template<typename T1, typename T2>
+//void test2(Hero<T1, T2> &h) {
+//	h.ShowHero();
+//}
+//
+////3、类模板化
+//template<typename T>
+//void test3(T &h) {
+//	h.ShowHero();
+//}
+//
+//
+//int main() {
+//
+//	Hero<string , double> h1("jett" , 100.0);
+//
+//	test1(h1);
+//	test2(h1);
+//	test3(h1);
+//
+//	return 0;
+//}
 
-template<class NameType, class HpType>
+/*
+* 四、类模板的继承
+*	1、直接指定类型
+*	2、类模板化
+*/
+template<class NameType , class HpType>
 class Hero {
 public:
 	Hero(NameType name , HpType hp) : m_name(name) , m_hp(hp) {
@@ -112,31 +157,43 @@ private:
 	HpType m_hp;
 };
 
-//1、直接指定类型
-void test1(Hero<string , double> &h) {
-	h.ShowHero();
-}
+// 1、直接指定类型
+template <class T1>
+class HeroSon :public Hero<string , double> {
+public:
+	HeroSon(string name , double hp , T1 age) :Hero<string , double>(name , hp) , m_age(age) {
+	}
+	void ShowAge() {
+		cout << "Age: " << m_age << endl;
+	}
+private:
+	T1 m_age;
+};
 
-//2、参数模板化
-template<typename T1, typename T2>
-void test2(Hero<T1, T2> &h) {
-	h.ShowHero();
-}
+// 2、类模板化
+template <class T1, class T2, class T3>
+class HeroSon2 :public Hero<T1 , T2> {
+public:
+	HeroSon2(T1 name , T2 hp , T3 age) :Hero<T1 , T2>(name , hp) , m_age(age) {
+	}
+	void ShowAge() {
+		cout << "Age: " << m_age << endl;
+	}
+private:
+	T3 m_age;
 
-//3、类模板化
-template<typename T>
-void test3(T &h) {
-	h.ShowHero();
-}
+};
 
 
 int main() {
 
-	Hero<string , double> h1("jett" , 100.0);
+	HeroSon<int> h1("jett" , 150.0 , 18);
+	h1.ShowHero();
+	h1.ShowAge();
 
-	test1(h1);
-	test2(h1);
-	test3(h1);
+	HeroSon2<string, double, int> h2("Omen" , 150.0 , 19);
+	h2.ShowHero();
+	h2.ShowAge();
 
 	return 0;
 }
